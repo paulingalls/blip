ea8ac1ab56bbc7344fd2077edda9d84f4acb5e0e
paulingalls <paul@paulingalls.com>
2021-03-20 19:20:02 -0700

    See if notification just works..:)

--- app/models/bag_location.rb
+++ app/models/bag_location.rb
@@ -1,6 +1,18 @@
+# frozen_string_literal: true
+
 class BagLocation < ApplicationRecord
+  after_create :notify_customer
   belongs_to :event
   belongs_to :customer
   accepts_nested_attributes_for :customer
   validates :location, presence: true
+
+  def notify_customer
+    client = Twilio::REST::Client.new
+    client.messages.create(
+      from: ENV.fetch('TWILIO_PHONE_NUMBER'),
+      to: customer.phone_number,
+      body: 'Bag Location Created'
+    )
+  end
 end
