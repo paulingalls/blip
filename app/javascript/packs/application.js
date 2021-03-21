ac14d15fa1630c1679e3b08cb4599366f5084b7b
paulingalls <paul@paulingalls.com>
2021-03-21 16:38:39 -0700

    got rid of toastr, not really using it added bootstrap as a web pack

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,10 +7,10 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import "../stylesheets/application"
-
-global.toastr = require("toastr")
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+import 'bootstrap'
+import "../stylesheets/application"
